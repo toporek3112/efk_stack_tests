acaeab413b378bc4a315e6c8df60a875128f7fa7
Topor Karol <karol.topor@a1.at>
2022-02-22 11:43:10 +0100

    cleanup

---
+++ scripts/generate_partymaster_logs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+input="/home/karol/Documents/Projects/efk_stack_tests/partymaster_logs/request.log"
+output="/home/karol/Documents/Projects/efk_stack_tests/partymaster_logs/request_out.log"
+while IFS= read -r line
+do
+  echo "$line" >> $output
+  sleep 2
+done < "$input"
