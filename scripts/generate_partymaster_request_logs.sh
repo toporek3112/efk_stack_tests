3fa53ade4cf33dac665a883103ce992187ca1f08
Topor Karol <karol.topor@a1.at>
2022-02-28 12:56:44 +0100

    logs from multiple servers

--- scripts/generate_partymaster_request_logs.sh
+++ scripts/generate_partymaster_request_logs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-input="/home/karol/Documents/Projects/efk_stack_tests/partymaster_logs/request.log"
-output="/home/karol/Documents/Projects/efk_stack_tests/partymaster_logs/request_out.log"
+input="/home/karol/Documents/Projects/efk_stack_tests/partymaster_logs/$1"
+output="/home/karol/Documents/Projects/efk_stack_tests/partymaster_logs/$2"
 while IFS= read -r line
 do
   echo "$line" >> $output
-  sleep 1
+  echo $line
+  sleep 0.2
 done < "$input"
